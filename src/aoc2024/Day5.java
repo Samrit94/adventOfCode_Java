f30c42f416d7165c96e085fe8b4398458b2d6851
Anne <anne-marie_mueller94@gmx.de>
2024-12-05 19:52:48 +0100

    Day5 Solution

--- src/aoc2024/Day5.java
+++ src/aoc2024/Day5.java
@@ -1,12 +1,158 @@
 package aoc2024;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class Day5 {
-	public static void main(String[] args) {
-		//Preparation File
+    public static void main(String[] args) {
+        List<String> rule_list = new ArrayList<String>();
+        List<String> page_list = new ArrayList<String>();
+        List<String> valid_list = new ArrayList<String>();
+        List<String> inValid_list = new ArrayList<String>();
+        
         // Path to input file
-        String filePath = "input.txt";
+        String filePath = "input.txt";        
+        readFile(filePath, rule_list, page_list);
         
-        System.out.println("Count output: ");
+        //Part1
+        getSortLists( rule_list, page_list,valid_list, inValid_list);        
+        int resultPart1 = sumMiddleValue(valid_list);
+        System.out.println("Sum of Valid Numebrs in the Middle: "+resultPart1);        
         
+        //Part2
+        Map<Integer, Rule> rules = new HashMap<>();
+        for (int i = 0; i< rule_list.size(); i++) {           
+            //read through the rules 
+            int[] lineNumbers = Arrays.stream(rule_list.get(i).split("\\|")).mapToInt(str -> Integer.parseInt(str.trim())).toArray();
+            if (!rules.containsKey(lineNumbers[0])) { rules.put(lineNumbers[0], new Rule(lineNumbers[0])); }
+            if (!rules.containsKey(lineNumbers[1])) { rules.put(lineNumbers[1], new Rule(lineNumbers[1])); }
+            
+            rules.get(lineNumbers[0]).second.add(lineNumbers[1]);
+            rules.get(lineNumbers[1]).first.add(lineNumbers[0]);            
+        }
+        
+        int sumPartTwo = 0; 
+       
+        for (int i = 0; i< inValid_list.size(); i++) {
+            String temp = inValid_list.get(i);      
+            int[] line = Arrays.stream(temp.split(",")).mapToInt(str -> Integer.parseInt(str.trim())).toArray();
+            Rule[] sort = Arrays.stream(line).mapToObj(p -> rules.get(p)).toArray(Rule[]::new);
+            Arrays.sort(sort);
+            sumPartTwo += sort[sort.length / 2].rule;
+        }
+        System.out.println("Sum of middle page numbers after sorting: " + sumPartTwo); 
     }
+    
+
+    static class Rule implements Comparable<Rule> {
+        int rule;
+        List<Integer> first = new ArrayList<>();
+        List<Integer> second = new ArrayList<>();
+        
+        public Rule(int rule) {
+            this.rule = rule;
+        }
+
+        @Override
+        public int compareTo(Rule o) {
+            if (this.first.contains(o.rule)) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    }
+    
+    public static int getMiddleValue(String[] line) {
+        int size = line.length;        
+        int indexAtHalf = size /2;
+        return Integer.parseInt(line[indexAtHalf]);
+    }
+    
+    public static int sumMiddleValue(List<String> valid) {
+        //count Middle values
+        int validSize = valid.size();
+        int sum=0;
+        for (int row = 0; row < validSize; row++) {
+            
+            String[] line = valid.get(row).split(",");
+            float lineSize = line.length;
+            //System.out.println("Size:"+lineSize);
+            int indexAtHalf = (int) Math.ceil(lineSize / 2);
+            //System.out.println("Num:"+line[indexAtHalf]);
+            sum +=  Integer.parseInt(line[indexAtHalf-1]);
+        }
+        
+        return sum;
+    }
+    
+    public static void getSortLists(List<String> rules, List<String> pages, List<String> valid, List<String> inValid) {
+        int pageSize = pages.size();
+        boolean isValid = true;
+        
+        for (int i = 0; i < pageSize; i++ ){
+            String[] line = pages.get(i).split(",");
+            int indexMax = line.length;            
+            for (int j = indexMax-1; j >= 0; j--) {
+                String checkValue = "|"+line[j];
+                //System.out.println("Checkvalue:"+checkValue);
+                for (int rule = 0; rule < rules.size(); rule++) {
+                    String checkAgainst = rules.get(rule);
+                    //System.out.println("checkAgainst:"+checkAgainst);
+                    if (checkAgainst.contains(checkValue)) {
+                        //System.out.println("checkAgainst:"+checkAgainst);
+                        for (int k = j; k < indexMax ; k++) {
+                            if (checkAgainst.contains(line[k]+"|")){
+                                isValid = false;
+                                break;
+                            }
+                        }                        
+                    }    
+                }
+            }
+            
+            if (isValid) {
+                valid.add(pages.get(i));
+            }
+            else {
+                inValid.add(pages.get(i));
+            }        
+            isValid = true;
+        }                   
+    }
+    
+    
+    public static void readFile(String filename, List<String> rules, List<String> pages) {
+        try
+        {
+                BufferedReader reader = new BufferedReader(new FileReader(filename));
+                String line;
+                //Read from file
+                while ((line = reader.readLine()) != null)
+                {
+                    if (line.contains("|")){
+                        rules.add(line);
+                    }
+                    else if(line.contains(",")) {
+                        pages.add(line);
+                    }
+                }
+                reader.close();
+                
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }        
+    }
+
+
+
 }
+
---
+++ src/aoc2024/Day6.java
@@ -0,0 +1,12 @@
+package aoc2024;
+
+public class Day6 {
+    public static void main(String[] args) {
+        //Preparation File
+        // Path to input file
+        String filePath = "input.txt";
+        
+        System.out.println("Count output: ");
+        
+    }
+}
