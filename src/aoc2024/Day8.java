01727d3288852f25c464f9489b053e0c58550fc2
Anne <anne-marie_mueller94@gmx.de>
2024-12-08 10:05:56 +0100

    Day 8 Solution

--- src/aoc2024/Day8.java
+++ src/aoc2024/Day8.java
@@ -1,12 +1,172 @@
 package aoc2024;
+import java.io.*;
+import java.util.*;
 
 public class Day8 {
+    
     public static void main(String[] args) {
-        //Preparation File
-        // Path to input file
-        String filePath = "input.txt";
+        String filename = "input.txt";
         
-        System.out.println("Count output: ");
+        int part1Result = readFileForPart1(filename);
+        System.out.println("Number of Unique Locations Part1: "+part1Result);
         
+        int part2Result = readFileForPart2(filename);
+        System.out.println("Number of Unique Locations Part2: "+part2Result);
     }
+    
+//--------------------Part 1 -------------------------    
+    public static int readFileForPart1(String filename) {
+        List<String> map = new ArrayList<>();
+        Map<Character, List<Point>> antennaTypeToLocations = new HashMap<>();
+        Set<Point> antinodes = new HashSet<>();
+        
+        try (BufferedReader file = new BufferedReader(new FileReader(filename))) {
+            String line;
+            //read file into a String List and Map Type including Class Point
+            while ((line = file.readLine()) != null) {
+                map.add(line);
+                for (int idx = 0; idx < line.length(); idx++) {
+                    char c = line.charAt(idx);
+                    if (c != '.') {
+                        antennaTypeToLocations
+                            .computeIfAbsent(c, k -> new ArrayList<>())
+                            .add(new Point(map.size() - 1, idx));
+                    }
+                }
+            }
+            
+            //figuring out unique locations according to instructions through loops
+            for (Map.Entry<Character, List<Point>> entry : antennaTypeToLocations.entrySet()) {
+                List<Point> locations = entry.getValue();
+                for (int i = 0; i < locations.size(); i++) {
+                    for (int j = i + 1; j < locations.size(); j++) {
+                        Point p1 = locations.get(i);
+                        Point p2 = locations.get(j);
+                        Point delta = new Point(p1.row - p2.row, p1.col - p2.col);
+                        Point an1 = new Point(p1.row + delta.row, p1.col + delta.col);
+                        Point an2 = new Point(p2.row - delta.row, p2.col - delta.col);
+
+                        if (inMap(an1, map)) antinodes.add(new Point(p1.row + delta.row, p1.col + delta.col));
+                        if (inMap(an2, map)) antinodes.add(new Point(p2.row - delta.row, p2.col - delta.col));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return antinodes.size();
+    }
+
+    static boolean inMap(Point p, List<String> map) {
+        return p.row >= 0 && p.row < map.size() && p.col >= 0 && p.col < map.get(0).length();
+    }
+
+
+//--------------------Part 2 -------------------------        
+    public static int readFileForPart2(String filename) {
+        List<String> map = new ArrayList<>();
+        Map<Character, List<Point>> antennaTypeToLocations = new HashMap<>();
+        Set<Point> antinodes = new HashSet<>();
+        
+        try (BufferedReader file = new BufferedReader(new FileReader(filename))) {
+            String line;
+            //read file into a String List and Map Type including Class Point
+            while ((line = file.readLine()) != null) {
+                map.add(line);
+                for (int idx = 0; idx < line.length(); idx++) {
+                    char c = line.charAt(idx);
+                    if (c != '.') {
+                        antennaTypeToLocations
+                            .computeIfAbsent(c, k -> new ArrayList<>())
+                            .add(new Point(map.size() - 1, idx));
+                    }
+                }
+            }
+
+          //figuring out unique locations according to new instructions
+            //gotta be honest, googled this until I understood what they wanted, still don't understand them fully...
+            for (Map.Entry<Character, List<Point>> entry : antennaTypeToLocations.entrySet()) {
+                List<Point> locations = entry.getValue();
+                for (int i = 0; i < locations.size(); i++) {
+                    for (int j = i + 1; j < locations.size(); j++) {
+                        Point p1 = locations.get(i);
+                        Point p2 = locations.get(j);
+                        Point delta = new Point(p1.row - p2.row, p1.col - p2.col);
+                        int hcf = findHCF(Math.abs(delta.row), Math.abs(delta.col));
+                        delta.row /= hcf;
+                        delta.col /= hcf;
+                        
+                        boolean added = true;
+                        int n = 0;
+                        while (added) {
+                            added = false;
+                            Point an1 = new Point(p1.row + n * delta.row, p1.col + n * delta.col);
+                            if (inMap(an1, map)) {
+                                antinodes.add(an1);
+                                added = true;
+                            }
+                            Point an2 = new Point(p2.row - n * delta.row, p2.col - n * delta.col);
+                            if (inMap(an2, map)) {
+                                antinodes.add(an2);
+                                added = true;
+                            }
+                            n++;
+                        }
+                    }
+                }
+            }
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return antinodes.size();
+    }
+
+    static int findHCF(int n1, int n2) {
+        if (n1 == 0) return n2;
+        if (n2 == 0) return n1;
+        if (n1 == n2) return n1;
+        if (n1 > n2) return findHCF(n1 - n2, n2);
+        else return findHCF(n1, n2 - n1);
+    }
+
+//---------------------helping Classes-------------------------       
+    //Point for Antenna location
+    static class Point {
+        int row;
+        int col;
+
+        Point(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return row == point.row && col == point.col;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(row, col);
+        }
+
+        @Override
+        public String toString() {
+            return "(" + row + "," + col + ")";
+        }
+    }
+    
+    //Hasher to get hash values from location
+    static class Hasher {
+        public int hashCode(Point p) {
+            return Objects.hash(p.row, p.col);
+        }
+    }
+    
+    
 }
--- src/aoc2024/Temp.java
+++ src/aoc2024/Temp.java
@@ -3,48 +3,70 @@ package aoc2024;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class Temp {  
 	//Test Class to test out codeparts
     public static void main(String[] args) {
-    	String filePath = "test.txt";
-        char [][] twoDimesionArray =  readFile(filePath);
-
+    	String filename = "test.txt";
+    	 List<String> map = new ArrayList<>();
+         Map<Character, List<Point>> antennaTypeToLocations = new HashMap<>();
+         
+         try (BufferedReader file = new BufferedReader(new FileReader(filename))) {
+             String line;
+             //read file into a String List and Map Type including Class Point
+             while ((line = file.readLine()) != null) {
+                 map.add(line);
+                 for (int idx = 0; idx < line.length(); idx++) {
+                     char c = line.charAt(idx);
+                     if (c != '.') {
+                         antennaTypeToLocations
+                             .computeIfAbsent(c, k -> new ArrayList<>())
+                             .add(new Point(map.size() - 1, idx));
+                     }
+                 }
+             }
+             
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
     }
 	
-	
-    public static char[][] readFile(String filename) {
-    	ArrayList array = new ArrayList();
-        char [][] twoDimesionArray = null;
-        try
-        {
-                BufferedReader reader = new BufferedReader(new FileReader(filename));
-                String data;
-                //Read from file
-                while ((data = reader.readLine()) != null)
-                {
-                    //Convert data to char array and add into array
-                    array.add(data.toCharArray());
-                }
-                reader.close();
+    static class Point {
+        int row;
+        int col;
 
-                //Creating a 2D char array base on the array size
-                twoDimesionArray = new char [array.size()][];
-
-                //Convert array from ArrayList to 2D array
-                for (int i = 0; i < array.size(); i++)
-                {
-                    twoDimesionArray[i] = (char [])array.get(i);
-                }
-                
+        Point(int row, int col) {
+            this.row = row;
+            this.col = col;
         }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }        
-		return twoDimesionArray;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return row == point.row && col == point.col;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(row, col);
+        }
+
+        @Override
+        public String toString() {
+            return "(" + row + "," + col + ")";
+        }
     }
     
-
-
+    static class Hasher {
+        public int hashCode(Point p) {
+            return Objects.hash(p.row, p.col);
+        }
+    }
+    
 }
