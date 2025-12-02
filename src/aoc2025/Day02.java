8ff9f9f1e29ee4c79d4df35d862fc54a4f7ff9f4
Anne <anne-marie_mueller94@gmx.de>
2025-12-02 09:22:44 +0100

    AoC 2025 Day 2 Solution

--- src/aoc2025/Day02.java
+++ src/aoc2025/Day02.java
@@ -1,14 +1,58 @@
 package aoc2025;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 
 public class Day02 {
 
     public static void main(String[] args) {
-        //Preparation File
-        // Path to input file
-        String filePath = "input.txt";
         
-        System.out.println("Count output: ");
+        String filePath = "input.txt" ;
+        String[] input;
+        
+        try (BufferedReader rdr = new BufferedReader(new FileReader(filePath))) {
+            ArrayList<String> fileLines = new ArrayList<>();
+            String line;
+
+            while((line = rdr.readLine()) != null) {
+                fileLines.add(line);
+            }
+
+            input = fileLines.toArray(new String[0]);
+            
+            //Count when copy found two times
+            String regexPart1 = "^([1-9]\\d*)\\1$";
+            CountInvalids(input, regexPart1);
+            
+            //Count when copy found more than two times -> RegEx 1+
+            String regexPart2 = "^([1-9]\\d*)\\1+$";
+            CountInvalids(input, regexPart2);
+            
+        } catch (IOException e) {
+            System.out.println("File not found.");
+        }
         
     }
+    
+    public static void CountInvalids(String[] input, String pattern) {
+        String[] idLine = input[0].split(",");
+        long sum = 0L;
+        for(String id : idLine) {
+            long start = Long.parseLong(id.split("-")[0]);
+            
+            long end = Long.parseLong(id.split("-")[1]);
+            
+            for(long i = start; i <= end; i++) {
+                if(String.valueOf(i).matches(pattern)) {
+                    sum += i;
+                }
+            }
+        }
+        
+        System.out.println("Count of invalid IDs: " + sum);
+    }
+    
 }
