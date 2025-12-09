ec936c260fef3971dbd6469cc0a503e70bd7f986
Anne <anne-marie_mueller94@gmx.de>
2025-12-09 14:10:56 +0100

    AoC 2025 Day 8 and 9 Solutions

--- src/aoc2025/Day08.java
+++ src/aoc2025/Day08.java
@@ -1,14 +1,237 @@
 package aoc2025;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class Day08 {
 
     public static void main(String[] args) {
-        //Preparation File
-        // Path to input file
+
         String filePath = "input.txt";
-        
-        System.out.println("Count output: ");
-        
+        ArrayList<JunctionBox> boxes = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            long largestSize = 0;
+            long result = 0;
+            String lineIn;
+
+            while ((lineIn = br.readLine()) != null) {
+                String[] parts = lineIn.split(",");
+                long x = Long.parseLong(parts[0].trim());
+                long y = Long.parseLong(parts[1].trim());
+                long z = Long.parseLong(parts[2].trim());
+
+                JunctionBox newBox = new JunctionBox(x, y, z);
+                boxes.add(newBox);
+            }
+
+            int connections = 0;
+            boolean[] connected = new boolean[boxes.size()];
+
+            ArrayList<Circuit> circuits = new ArrayList<>();
+            JunctionBox box1 = null;
+            JunctionBox box2 = null;
+
+            int idBox1 = -1;
+            int idBox2 = -1;
+
+            double minDistance = 0;
+            boolean allConnected = false;
+
+            while (!allConnected) {
+                double shortest = Double.MAX_VALUE;
+
+                for (int i = 0; i < boxes.size() - 1; i++) {
+                    for (int j = i + 1; j < boxes.size(); j++) {
+                        double distance = boxes.get(i).getDistance(boxes.get(j));
+
+                        if (distance < shortest && distance > minDistance) {
+                            shortest = distance;
+                            box1 = boxes.get(i);
+                            box2 = boxes.get(j);
+                            idBox1 = i;
+                            idBox2 = j;
+                        }
+                    }
+                }
+
+                if (connected[idBox1] == false && connected[idBox2] == false) {                    
+                    Circuit newCircuit = new Circuit();
+                    newCircuit.addMember(box1);
+                    newCircuit.addMember(box2);
+                    
+                    minDistance = shortest;
+                    circuits.add(newCircuit);
+                    connections++;
+                    
+                    connected[idBox1] = true;
+                    connected[idBox2] = true;
+                    
+                } else if (connected[idBox1] == true && connected[idBox2] == false) {
+                    int circuitIndex = 0;
+
+                    for (int i = 0; i < circuits.size(); i++) {
+                        if (circuits.get(i).hasMember(box1)) {
+                            circuitIndex = i;
+                        }
+                    }
+
+                    circuits.get(circuitIndex).addMember(box2);
+                    connections++;
+                    
+                    connected[idBox2] = true;
+                    minDistance = shortest;
+                    
+                } else if (connected[idBox1] == false && connected[idBox2] == true) {
+                    int circuitIndex = 0;
+
+                    for (int i = 0; i < circuits.size(); i++) {
+                        if (circuits.get(i).hasMember(box2)) {
+                            circuitIndex = i;
+                        }
+                    }
+
+                    circuits.get(circuitIndex).addMember(box1);
+                    connections++;
+                    
+                    connected[idBox1] = true;
+                    minDistance = shortest;
+                    
+                } else if (connected[idBox1] == true && connected[idBox2] == true) {
+                    int circuitIndex1 = 0;
+                    int circuitIndex2 = 0;
+
+                    for (int i = 0; i < circuits.size(); i++) {
+                        if (circuits.get(i).hasMember(box1)) {
+                            circuitIndex1 = i;
+                        }
+                        if (circuits.get(i).hasMember(box2)) {
+                            circuitIndex2 = i;
+                        }
+                    }
+
+                    if (circuitIndex1 != circuitIndex2) {
+                        for (int i = 0; i < circuits.get(circuitIndex2).getMembers(); i++) {
+                            circuits.get(circuitIndex1).addMember(circuits.get(circuitIndex2).members.get(i));
+                        }
+
+                        circuits.remove(circuitIndex2);
+                        connections++;
+                        minDistance = shortest;
+                    } else if (circuitIndex1 == circuitIndex2) {
+                        minDistance = shortest;
+                        connections++;
+                    }
+                }
+
+                if (circuits.size() == 1) {
+                    allConnected = true;
+
+                    for (int i = 0; i < connected.length; i++) {
+                        if (!connected[i]) {
+                            allConnected = false;
+                            break;
+                        }
+                    }
+
+                    if (allConnected) {
+                        result = box1.getX() * box2.getX();
+                        System.out.println("Mulitply X of last two boxes: " + result);
+                        break;
+                    }
+                }
+
+                box1 = null;
+                box2 = null;
+
+                if (connections == 1000) {
+                    Circuit[] toSort = new Circuit[circuits.size()];
+
+                    for (int i = 0; i < circuits.size(); i++) {
+                        toSort[i] = circuits.get(i);
+                    }
+
+                    for (int i = 1; i < toSort.length; i++) {
+                        Circuit temp = toSort[i];
+                        int index = i;
+
+                        while (index > 0 && temp.compareTo(toSort[index - 1]) > 0) {
+                            toSort[index] = toSort[index - 1];
+                            index--;
+                        }
+
+                        toSort[index] = temp;
+                    }
+
+                    largestSize = toSort[0].getMembers() * toSort[1].getMembers() * toSort[2].getMembers();
+                    System.out.println("Size of the three largest circuits: " + largestSize);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("File not found.");
+        }
+    }
+
+    public static class JunctionBox {
+        private long x;
+        private long y;
+        private long z;
+
+        public JunctionBox(long x, long y, long z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public long getX() {
+            return x;
+        }
+
+        public long getY() {
+            return y;
+        }
+
+        public long getZ() {
+            return z;
+        }
+
+        public double getDistance(JunctionBox other) {
+            return Math.sqrt(Math.pow(this.getX() - other.getX(), 2) + Math.pow(this.getY() - other.getY(), 2)
+                    + Math.pow(this.getZ() - other.getZ(), 2));
+        }
+    }
+
+    public static class Circuit {
+        public ArrayList<JunctionBox> members;
+
+        public Circuit() {
+            this.members = new ArrayList<JunctionBox>();
+        }
+
+        public long getMembers() {
+            return members.size();
+        }
+
+        public void addMember(JunctionBox other) {
+            members.add(other);
+        }
+
+        public boolean hasMember(JunctionBox input) {
+            if (members.contains(input))
+                return true;
+            return false;
+        }
+
+        public int compareTo(Circuit other) {
+            if (this.getMembers() > other.getMembers()) {
+                return 1;
+            }
+            if (this.getMembers() == other.getMembers()) {
+                return 0;
+            }
+            return -1;
+        }
     }
 }
--- src/aoc2025/Day09.java
+++ src/aoc2025/Day09.java
@@ -1,14 +1,112 @@
 package aoc2025;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day09 {
 
     public static void main(String[] args) {
-        //Preparation File
-        // Path to input file
         String filePath = "input.txt";
         
-        System.out.println("Count output: ");
+        try( BufferedReader br = new BufferedReader(new FileReader(filePath))){
+            
+            long largestArea = 0;
+            String line;
+            List<Coordinate> coords = new ArrayList<>();
+            List<Coord> coordsPart2 = new ArrayList<>();
+            
+            while ((line = br.readLine()) != null) {
+                String[] coordinate = line.split(",");
+                coords.add(new Coordinate(Integer.parseInt(coordinate[0]), Integer.parseInt(coordinate[1])));
+                //For Part2
+                coordsPart2.add(Coord.fromStr(line));
+            }
+            
+            for (int i = 0; i < coords.size(); i++) {
+                for (int k = i + 1; k < coords.size(); k++) {
+                    Coordinate first = coords.get(i);
+                    Coordinate second = coords.get(k);
+                    long size = (long) (Math.abs(first.x - second.x) + 1) * (Math.abs(first.y - second.y) + 1);
+                    if (size > largestArea) {
+                        largestArea = size;
+                    }
+                }
+            }
+            
+            System.out.println("Largest Area: " + largestArea);        
+            
+            //Part 2 had to google a different way to solve it...            
+            List<Rectangle> rectangles = new ArrayList<>();
+            List<Rectangle> lines = new ArrayList<>();
+
+           //build all rectangles from every pair of points
+            for (int i = 0; i < coordsPart2.size() - 1; i++) {
+                for (int j = i + 1; j < coordsPart2.size(); j++) {
+                    rectangles.add(Rectangle.fromCoords(coordsPart2.get(i), coordsPart2.get(j)));
+                }
+            }
+
+            //build rectangles from consecutive points 
+            for (int i = 0; i < coordsPart2.size(); i++) {
+                int nextIndex = (i + 1) % coordsPart2.size(); // wrap around
+                lines.add(Rectangle.fromCoords(coordsPart2.get(i), coordsPart2.get(nextIndex)));
+            }
+
+            //find the largest rectangle area that does not overlap
+            long maxArea = rectangles.stream()
+                              .filter(r -> lines.stream().noneMatch(r::isOverlap))
+                              .mapToLong(Rectangle::area)
+                              .max()
+                              .orElseThrow();
+
+            System.out.println("Largest Area with only red and green tiles: " + maxArea);            
+            
+        } catch (IOException e) {
+            System.out.println("File not found.");
+        }     
+
         
     }
+    
+    public static class Coordinate {
+        public long x;
+        public long y;
+
+        public Coordinate(long x, long y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void set(long x, long y) {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+    
+    //Needed for Part 2
+    public record Coord(long x, long y) {
+        static Coord fromStr(String str) {
+            var parts = str.split(",");
+            return new Coord(Long.parseLong(parts[0]), Long.parseLong(parts[1]));
+        }
+    }
+    
+    public record Rectangle(Coord min, Coord max) {
+        boolean isOverlap(Rectangle other) {
+            return min.x < other.max.x && max.x > other.min.x && min.y < other.max.y && max.y > other.min.y;
+        }
+    
+        long area() {
+            return (max.x - min.x + 1) * (max.y - min.y + 1);
+        }
+    
+        static Rectangle fromCoords(Coord a, Coord b) {
+            return new Rectangle(new Coord(Math.min(a.x, b.x), Math.min(a.y, b.y)), new Coord(Math.max(a.x, b.x), Math.max(a.y, b.y)));
+        }
+    }
+    
 }
